5ffb7fe716eb979e7f0859d0890b8cb307d4a0cd
MagnusLj <magnusljungvall@hotmail.com>
2019-12-08 13:35:29 +0000

    New round with CI things

--- config/keys.php
+++ config/keys.php
@@ -3,7 +3,7 @@
  * Config file for url.
  */
 return [
-    // "darkskyKey" => "1c775c0ccd4c4690b4710b8e2e20bf1d",
-    "darkskyKey" => "758ef0730814daf11051a6e33063a6ed",
+    "darkskyKey" => "1c775c0ccd4c4690b4710b8e2e20bf1d",
+    // "darkskyKey" => "758ef0730814daf11051a6e33063a6ed",
     "ipstackKey" => "d1efc4cc23a8b14dfad565ee6bde80b8",
 ];
